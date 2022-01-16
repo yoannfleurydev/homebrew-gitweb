119636d77805050c608969456ec01b1f06614544
Yoann Fleury <yoann.fleury@yahoo.com>
2022-01-16 11:23:06 +0100

    gitweb 0.3.4

--- Formula/gitweb.rb
+++ Formula/gitweb.rb
@@ -1,8 +1,8 @@
 class Gitweb < Formula
   desc "🌲 Open the current remote repository in your browser"
   homepage "https://github.com/yoannfleurydev/gitweb"
-  url "https://github.com/yoannfleurydev/gitweb/archive/v0.3.2.tar.gz"
-  sha256 "484b1c56973e06c2ad7b33ccdaafa8ef6fba236681c7f8cd04027738b5335cd3"
+  url "https://github.com/yoannfleurydev/gitweb/archive/v0.3.4.tar.gz"
+  sha256 "6cde36b6d52c1881057a3f1bda4fad48a228e57461809013fb9147a0de6e6a8c"
   license "Apache-2.0"
 
   depends_on "openssl@1.1" => :build
@@ -13,6 +13,6 @@ class Gitweb < Formula
   end
 
   test do
-    assert_equal "gitweb 0.3.2", shell_output("#{bin}/gitweb -h").lines.first.strip
+    assert_equal "gitweb 0.3.4", shell_output("#{bin}/gitweb -h").lines.first.strip
   end
 end
